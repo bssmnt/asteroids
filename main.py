23a34fc296180b75b01d25b907da1b1a2633b1c1
bssmnt <socuting@gmail.com>
2024-09-10 16:23:59 +0100

    third commit

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    
-
     black_bg = (0, 0, 0)
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     
     while True:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(black_bg)
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
         
-
-
 if __name__ == "__main__":
     main()
